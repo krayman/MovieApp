a6a9109e5a3d8ce7bc7727b12d72c4be02bd1978
krayman <50882897+krayman@users.noreply.github.com>
2021-03-31 11:30:11 +0700

    Встроил gmaps

--- app/src/main/java/com/krayapp/movieapp/ui/main/MapsFragment.kt
+++ app/src/main/java/com/krayapp/movieapp/ui/main/MapsFragment.kt
@@ -3,10 +3,7 @@ package com.krayapp.movieapp.ui.main
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Geocoder
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
+import android.location.*
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -23,6 +20,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.krayapp.movieapp.R
+import com.krayapp.movieapp.databinding.FragmentMapsBinding
+import com.krayapp.movieapp.databinding.GmapFragBinding
 import kotlinx.android.synthetic.main.gmap_frag.*
 import java.io.IOException
 
@@ -30,26 +29,33 @@ private const val REQUEST_CODE = 12345
 private const val REFRESH_PERIOD = 60000L
 private const val MINIMAL_DISTANCE = 100f
 class MapsFragment : Fragment() {
-    private val onLocationListener = object : LocationListener {
 
-        override fun onLocationChanged(location: Location) {
-            context?.let {
-                getAddressAsync(it, location)
-            }
-        }
-    }
+    private var _binding: GmapFragBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var map: GoogleMap
     private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+        val initialPlace = LatLng(52.52000659999999, 13.404953999999975)
+        googleMap.addMarker(
+            MarkerOptions().position(initialPlace).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(initialPlace))
+        activateMyLocation(googleMap)
+    }
+
+    /*private val callback = OnMapReadyCallback { googleMap ->
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        _binding = GmapFragBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -57,14 +63,30 @@ class MapsFragment : Fragment() {
         checkPermission()
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-    }
+        initSearchByAddress()
 
+    }
+    private fun initSearchByAddress() {
+        binding.buttonSearch.setOnClickListener {
+            val geoCoder = Geocoder(it.context)
+            val searchText = searchAddress.text.toString()
+            Thread {
+                try {
+                    val addresses = geoCoder.getFromLocationName(searchText, 1)
+                    if (addresses.size > 0) {
+                        goToAddress(addresses, it, searchText)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
     private fun checkPermission() {
         activity?.let {
             when {
                 ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
                         PackageManager.PERMISSION_GRANTED -> {
-                    getLocation()
                 }
                 shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
                     showRationaleDialog()
@@ -94,46 +116,31 @@ class MapsFragment : Fragment() {
             REQUEST_CODE
         )
     }
-    private fun getLocation() {
-        activity?.let { context ->
-            if (ContextCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
-                val locationManager =
-                    context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                    val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
-                    provider?.let {
-                        locationManager.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER,
-                            REFRESH_PERIOD,
-                            MINIMAL_DISTANCE,
-                            onLocationListener
-                        )
-                    }
-                } else {
-                    val location =
-                        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                    if (location == null) {
-                        showDialog(
-                            getString(R.string.dialog_title_gps_turned_off),
-                            getString(R.string.dialog_message_last_location_unknown)
-                        )
-                    } else {
-                        getAddressAsync(context, location)
-                        showDialog(
-                            getString(R.string.dialog_title_gps_turned_off),
-                            getString(R.string.dialog_message_last_known_location)
-                        )
-                    }
-                }
-            } else {
-                showRationaleDialog()
-            }
+    private fun goToAddress(
+        addresses: MutableList<Address>,
+        view: View,
+        searchText: String
+    ) {
+        val location = LatLng(
+            addresses[0].latitude,
+            addresses[0].longitude
+        )
+        view.post {
+            map.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location,
+                    15f
+                )
+            )
+        }
+    }
+    private fun activateMyLocation(googleMap: GoogleMap) {
+        context?.let {
+            val isPermissionGranted =
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED
+            googleMap.isMyLocationEnabled = isPermissionGranted
+            googleMap.uiSettings.isMyLocationButtonEnabled = isPermissionGranted
         }
     }
     private fun showDialog(title: String, message: String) {
@@ -146,23 +153,19 @@ class MapsFragment : Fragment() {
                 .show()
         }
     }
-    private fun getAddressAsync(
-        context: Context,
-        location: Location
-    ) {
-        val geoCoder = Geocoder(context)
-        Thread {
-            try {
-                val addresses = geoCoder.getFromLocation(
-                    location.latitude,
-                    location.longitude,
-                    1
-                )
-                textAddress.post { textAddress.text = addresses[0].getAddressLine(0) }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }.start()
+    private fun getAddressAsync(location: LatLng) {
+        context?.let {
+            val geoCoder = Geocoder(it)
+            Thread {
+                try {
+                    val addresses =
+                        geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+                    textAddress.post { textAddress.text = addresses[0].getAddressLine(0) }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
     }
     companion object{
         fun newInstance() = MapsFragment()
